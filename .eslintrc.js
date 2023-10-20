0cc81c3fee0507ab8a4cd90117fd3aa0e219ca0a
kangning.wkn <kangning.wkn@alibaba-inc.com>
2023-10-20 16:52:29 +0800

    目录结构

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+    rules: {
+        'prettier/prettier': 'warn',
+        '@typescript-eslint/no-explicit-any': ['off'],
+        '@typescript-eslint/explicit-function-return-type': ['off'],
+        '@typescript-eslint/explicit-module-boundary-types': ['off'],
+        '@typescript-eslint/no-non-null-assertion': ['off'],
+        '@typescript-eslint/interface-name-prefix': ['off'],
+        '@typescript-eslint/ban-types': ['off'],
+    },
+};
