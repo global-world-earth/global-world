bdc6c89e6d04b56772260694ce3418812fd05169
kangning.wkn <kangning.wkn@alibaba-inc.com>
2023-10-31 10:21:38 +0800

    渲染基类定义

--- src/render/earth.ts
+++ src/render/earth.ts
@@ -3,15 +3,27 @@ import { Mesh } from '../ts-type/util'
 import { sphere } from '../utils/geo/geo'
 import { View } from '../view/view'
 import { EllipseProjection } from '../utils/projection/global'
+import { Render } from '.'
 
-export class Earth {
+export class Earth extends Render {
     public view: View = new View();
     // 测试用球
     public interactiveSphere: Mesh;
 
     constructor(regl: REGL.Regl) {
+        super(regl);
         this.interactiveSphere = sphere(regl, [EllipseProjection.a, 0, 0], EllipseProjection.a);
     }
 
-  render() {}
+    public init(): void {
+
+    }
+
+    public render(): void {
+
+    }
+
+    public destroy(): void {
+
+    }
 }
---
+++ src/render/index.ts
@@ -0,0 +1,14 @@
+import REGL from "regl";
+
+export abstract class Render {
+    public regl: REGL.Regl;
+    constructor(regl: REGL.Regl) {
+        this.regl = regl;
+    }
+    // 初始化渲染指令
+    public abstract init(): void;
+    // 渲染
+    public abstract render(): void;
+    // 销毁资源
+    public abstract destroy(): void;
+}
